aaa99e9a3d59fa5d9798120ef3b03b1633942c18
katie-soft <analyst.meo@gmail.com>
2023-04-13 11:12:21 +0300

    feat: 2 tasks solved (count cats, dream team)

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  let count = 0;
+  arr.forEach(element => {
+    element.forEach(ears => {
+      if (ears === '^^') {
+        count++
+      }
+    })
+  });
+  return count;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    let result = '';
+    members.forEach(el => {
+      if (typeof el === 'string') {
+        result += el.trim()[0].toUpperCase();
+      }
+    })
+    return result.split('').sort().join('');
+  }
 }
 
 module.exports = {
